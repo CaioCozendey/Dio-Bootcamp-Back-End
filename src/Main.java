2fbab5f8009fc8a9c27192f78d72f391bdd15879
Caio Dias Cozendey <caiocozendey@hotmail.com>
2023-04-26 00:25:51 -0300

    Add Main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        //Mini calculadora
+        Calculadora.operacao(10, 10 , "+");
+
+        //Mensagem de acordo com a hora do  dia
+        Mensagem.mensagem(10);
+
+        //Empréstimo
         Emprestimo.calcular(10000, 36, 1);
 
     }
